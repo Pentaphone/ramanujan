ced56b70d35596e3488fedcb14c716f5c9a99a25
Pentaphone <154714553+Pentaphone@users.noreply.github.com>
2026-06-16 01:08:40 +0200

    Add exception handling

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+print("Ramanujan")
+print()
+
 import random
 
 operations = ["+","-","×","/"]
 
-print("Ramanujan")
-print()
 i = 1
 correct = 0
 
 while True:
-
+    
     operation = operations[random.randint(0, 3)]
     a,b = random.randint(-100, 100), random.randint(-100, 100)
     if a == 0: a = a+1
     if b == 0: b = b+1
     
-    result = 1
+    result = 0
     match operation:
         case "+":
             if b < 0: b = b * -1
@@ -34,11 +35,14 @@ while True:
             result = int(a / b)
             
     player_input = input(f"{a} {operation} {b} = ")
-    if int(player_input) == result:
-        correct = correct+1
-        print(f"Correct! ({correct}/{i})")
-    else:
-        print(f"Incorrect, its {result}.")
+    try:
+        if int(player_input) == result:
+            correct = correct+1
+            print(f"Correct! ({correct}/{i})")
+        else:
+            print(f"Incorrect, its {result}.")
+    except:
+        print(f"That's not a number. Please enter an integer.")
     print()
     
     i = i+1
